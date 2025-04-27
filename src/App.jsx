0a63d699eee270327b689c0c2abbd144b445b831
Hamzah Imtiaz <imtiazhamzah0311@gmail.com>
2025-04-27 20:27:50 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Weather from './components/Weather'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Weather/>
+      <footer className="footer">
+    © 2025 Weather App | Hamzah Imtiaz | All rights reserved
+  </footer>
+    </div>
+  )
+}
+
+export default App
